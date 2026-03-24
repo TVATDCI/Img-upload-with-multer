a5b4aaac0e664d282e26fbffcedb18e8ac6cb505
TNTHNGVDYNND <tuanthong.vaidyanond@gmail.com>
2026-03-24 15:29:11 +0100

    Add size, originalName, and displayName fields to Image schema

--- src/models/Image.js
+++ src/models/Image.js
@@ -3,9 +3,12 @@ import mongoose from 'mongoose';
 const imageSchema = new mongoose.Schema(
   {
     filename: { type: String, required: true },
+    originalName: { type: String },
+    displayName: { type: String },
     path: { type: String, required: true },
     publicId: { type: String },
     localPath: { type: String },
+    size: { type: Number },
     uploadDate: { type: Date, default: Date.now },
     user_ip: { type: String, required: true },
   },
